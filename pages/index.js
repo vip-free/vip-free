9e0fe013259fc579d51271fdb99e8ac82fa7bfea
o <89508077+vip-free@users.noreply.github.com>
2025-08-29 11:26:07 +0700

    Delete pages directory

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{textAlign: "center", padding: "50px"}}>
-      <h1>NYEXSOBATEN</h1>
-      <p>Website berhasil jalan 🚀</p>
-    </div>
-  );
-}
